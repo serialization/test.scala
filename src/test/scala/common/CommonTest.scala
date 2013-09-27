4fd254bf3fee4ecd634634b16e68e7e1674d304e
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-09-27 14:14:54 +0200

    moved some tests to scalatest

--- src/test/scala/common/CommonTest.scala
+++ src/test/scala/common/CommonTest.scala
@@ -1,12 +1,14 @@
 package common
 
 import java.io.File
-
 import scala.language.implicitConversions
-
 import org.scalatest.junit.AssertionsForJUnit
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 
-class CommonTest extends AssertionsForJUnit {
+@RunWith(classOf[JUnitRunner])
+class CommonTest extends FunSuite {
   protected implicit def nameToPath(s: String) = new File("src/test/resources/"+s).toPath
 
 }
---
+++ src/test/scala/date/ParseTest.scala
@@ -0,0 +1,88 @@
+package date
+
+import org.junit.Assert
+import common.CommonTest
+import org.junit.runner.RunWith
+import date.api.SkillState
+import date.internal.SkillException
+
+/**
+ * Tests the file reading capabilities.
+ */
+class ParseTest extends CommonTest {
+
+  test("two dates") {
+    val σ = SkillState.read("test.sf")
+
+    val it = σ.getDates;
+    Assert.assertEquals(1L, it.next.getDate)
+    Assert.assertEquals(-1L, it.next.getDate)
+    assert(!it.hasNext, "there shouldn't be any more elemnets!")
+  }
+
+  test("simple nodes") { Assert.assertNotNull(SkillState.read("node.sf")) }
+  test("simple test") { Assert.assertNotNull(SkillState.read("test.sf")) }
+  /**
+   * @see § 6.2.3.Fig.3
+   */
+  test("two node blocks") { Assert.assertNotNull(SkillState.read("twoNodeBlocks.sf")) }
+  /**
+   * @see § 6.2.3.Fig.4
+   */
+  test("colored nodes") { Assert.assertNotNull(SkillState.read("coloredNodes.sf")) }
+  test("four colored nodes") { Assert.assertNotNull(SkillState.read("fourColoredNodes.sf")) }
+  test("empty blocks") { Assert.assertNotNull(SkillState.read("emptyBlocks.sf")) }
+  test("two types") { Assert.assertNotNull(SkillState.read("twoTypes.sf")) }
+  test("trivial type definition") { Assert.assertNotNull(SkillState.read("trivialType.sf")) }
+
+  test("data chunk is too long") {
+    intercept[SkillException] {
+      SkillState.read("illformed/longerDataChunk.sf").getDates
+    }
+  }
+  test("data chunk is too short") {
+    intercept[SkillException] {
+      SkillState.read("illformed/shorterDataChunk.sf").getDates
+    }
+  }
+  test("incompatible field types") {
+    intercept[SkillException] {
+      SkillState.read("illformed/incompatibleType.sf").getDates
+    }
+  }
+  test("reserved type ID") {
+    intercept[SkillException] {
+      SkillState.read("illformed/illegalTypeID.sf").getDates
+    }
+  }
+  test("missing user type") {
+    intercept[SkillException] {
+      SkillState.read("illformed/missingUserType.sf").getDates
+    }
+  }
+  test("illegal string pool offset") {
+    intercept[SkillException] {
+      SkillState.read("illformed/illegalStringPoolOffsets.sf").getDates
+    }
+  }
+  test("missing field declarations in second block") {
+    intercept[SkillException] {
+      SkillState.read("illformed/missingFieldInSecondBlock.sf").getDates
+    }
+  }
+  test("duplicate type definition in the first block") {
+    intercept[SkillException] {
+      SkillState.read("illformed/duplicateDefinition.sf").getDates
+    }
+  }
+  test("append in the first block") {
+    intercept[SkillException] {
+      SkillState.read("illformed/duplicateDefinitionMixed.sf").getDates
+    }
+  }
+  test("duplicate append in the same block") {
+    intercept[SkillException] {
+      SkillState.read("illformed/duplicateDefinitionSecondBlock.sf").getDates
+    }
+  }
+}
