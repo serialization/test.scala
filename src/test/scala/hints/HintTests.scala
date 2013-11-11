0c550858bc93f01a9bd841b8e2d8299e33f03037
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-11-11 17:52:29 +0100

    added a test for ignored fields and types

---
+++ src/test/scala/hints/HintTests.scala
@@ -0,0 +1,28 @@
+package hints
+
+import scala.reflect.Manifest
+import org.scalatest.Engine
+import org.scalatest.events.Formatter
+import common.CommonTest
+import hints.ignore.api.SkillState
+
+class HintTests extends CommonTest {
+
+  test("access ignored field") {
+    val state = SkillState.read("test.sf")
+
+    val err = intercept[IllegalAccessError] {
+      state.getDates.next.date
+    }
+    assert(err.getMessage() === "date has an !ignore hint")
+  }
+
+  test("access ignored type") {
+    val state = SkillState.read("node.sf")
+
+    val err = intercept[IllegalAccessError] {
+      state.getNodes.next.node
+    }
+    assert(err.getMessage() === "node has a type with an !ignore hint")
+  }
+}
