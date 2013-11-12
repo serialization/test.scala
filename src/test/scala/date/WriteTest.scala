6b473f955968317a9d7b08c6cf37974ed98f2b11
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-11-12 12:17:22 +0100

    updated write test

--- src/test/scala/date/WriteTest.scala
+++ src/test/scala/date/WriteTest.scala
@@ -11,20 +11,15 @@ import java.io.File
 class WriteTest extends CommonTest {
 
   test("simple write date test") {
-    val f: File = File.createTempFile("writetest_", ".sf")
-
     val σ = SkillState.create
     σ.addDate(1)
     σ.addDate(-1)
-    σ.write("writetest-date-(1,-1).sf") // f.getAbsolutePath()
-
-    f.deleteOnExit()
+    σ.write(File.createTempFile("writetest", ".sf").toPath)
   }
 
-  ignore("simple write test") {
+  test("simple write test") {
     val σ = SkillState.create
     (1 to 100) foreach (σ.addDate(_))
-    σ.write("writetest-date-(1..100).sf")
+    σ.write(File.createTempFile("writetest", ".sf").toPath)
   }
-
 }
