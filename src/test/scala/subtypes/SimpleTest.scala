8eaa11702d1707e4857a54afc1d63f149cfed6f3
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-11-13 13:58:25 +0100

    added another subtyping test

--- src/test/scala/subtypes/SimpleTest.scala
+++ src/test/scala/subtypes/SimpleTest.scala
@@ -3,6 +3,7 @@ package subtypes
 import common.CommonTest
 import subtypes.api.SkillState
 import subtypes.internal.SerializableState
+import java.io.File
 
 class SimpleTest extends CommonTest {
 
@@ -14,5 +15,18 @@ class SimpleTest extends CommonTest {
   test("subtypes read") {
     val state = SkillState.read("localBasePoolStartIndex.sf")
     state.asInstanceOf[SerializableState].dumpDebugInfo
+  }
+
+  test("subtypes write") {
+    val file = File.createTempFile("writetest", ".sf")
+    val path = file.toPath
+
+    val state = SkillState.read("localBasePoolStartIndex.sf")
+
+    state.write(path)
+    val state2 = SkillState.read(path)
+    assert(state.getAs.map(_.getClass.getSimpleName).sameElements(state2.getAs.map(_.getClass.getSimpleName)))
+
+    fail("write in type order")
   }
 }
