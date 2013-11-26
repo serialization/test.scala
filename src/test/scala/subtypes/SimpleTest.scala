23ad3b543c5f6c2459686d30aef68d26d6a7619c
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2013-11-26 12:26:42 +0100

    improved subtypes read test

--- src/test/scala/subtypes/SimpleTest.scala
+++ src/test/scala/subtypes/SimpleTest.scala
@@ -9,12 +9,30 @@ class SimpleTest extends CommonTest {
 
   test("subtypes read foreign") {
     val state = SkillState.read("annotationTest.sf")
-    state.asInstanceOf[SerializableState].dumpDebugInfo
+    assert(null != state)
   }
 
-  test("subtypes read") {
+  test("subtypes read; see §6.3.2") {
     val state = SkillState.read("localBasePoolStartIndex.sf")
-    state.asInstanceOf[SerializableState].dumpDebugInfo
+    val types = "aabbbcbbddacd"
+
+    // check types
+    val actualTypes = state.getAs.map(_.getClass.getSimpleName.toLowerCase).mkString("");
+    assert(actualTypes === types, state.asInstanceOf[SerializableState].dumpDebugInfo)
+
+    // check fields (all fields are self-references)
+    state.getAs.foreach { instance ⇒
+      assert(instance.getA === instance)
+    }
+    state.getBs.foreach { instance ⇒
+      assert(instance.getB === instance)
+    }
+    state.getCs.foreach { instance ⇒
+      assert(instance.getC === instance)
+    }
+    state.getDs.foreach { instance ⇒
+      assert(instance.getD === instance)
+    }
   }
 
   test("subtypes write") {
