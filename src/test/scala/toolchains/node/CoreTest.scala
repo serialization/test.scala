b5ccb3d894c653171f36dab900d9bcf0e54cb607
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-01-14 17:03:25 +0100

    added more node tests

--- src/test/scala/toolchains/node/CoreTest.scala
+++ src/test/scala/toolchains/node/CoreTest.scala
@@ -74,6 +74,113 @@ class CoreTest extends CommonTest {
     }
   }
 
+  test("two toolchain cycles -- append") {
+    val path = tmpFile("nodeExample.with.strings")
+
+    locally {
+      val σ = Creator.create
+      σ.Node(23)
+      σ.Node(42)
+      σ.write(path)
+    }
+
+    locally {
+      import tool2.Node
+
+      val σ = ColorTool.read(path)
+      σ.String.add("red")
+      σ.String.add("black")
+      σ.String.add("grey")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.color = "red"
+        case n @ Node(42, _) ⇒ n.color = "black"
+        case n               ⇒ n.color = "grey"
+      }
+      σ.append
+    }
+
+    locally {
+      import tool3.Node
+
+      val σ = DescriptionTool.read(path)
+      σ.String.add("Some odd number.")
+      σ.String.add("The answer.")
+      σ.String.add("Boring!")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.description = "Some odd number."
+        case n @ Node(42, _) ⇒ n.description = "The answer."
+        case n               ⇒ n.description = "Boring!"
+      }
+      σ.append
+    }
+
+    locally {
+      val σ = Creator.read(path)
+      σ.Node(23)
+      σ.Node(42)
+      // not allowed: some fields are missing
+      val e = intercept[AssertionError] {
+        σ.append(path)
+      }
+      assert(e.getMessage() === "assertion failed: adding instances with an unknown field is currently not supported")
+    }
+  }
+
+  test("two toolchain cycles -- write") {
+    val path = tmpFile("nodeExample.with.strings")
+
+    locally {
+      val σ = Creator.create
+      σ.Node(23)
+      σ.Node(42)
+      σ.write(path)
+    }
+
+    locally {
+      import tool2.Node
+
+      val σ = ColorTool.read(path)
+      σ.String.add("red")
+      σ.String.add("black")
+      σ.String.add("grey")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.color = "red"
+        case n @ Node(42, _) ⇒ n.color = "black"
+        case n               ⇒ n.color = "grey"
+      }
+      σ.append
+    }
+
+    locally {
+      import tool3.Node
+
+      val σ = DescriptionTool.read(path)
+      σ.String.add("Some odd number.")
+      σ.String.add("The answer.")
+      σ.String.add("Boring!")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.description = "Some odd number."
+        case n @ Node(42, _) ⇒ n.description = "The answer."
+        case n               ⇒ n.description = "Boring!"
+      }
+      σ.append
+    }
+
+    locally {
+      val σ = Creator.read(path)
+      σ.Node(23)
+      σ.Node(42)
+      σ.write(path)
+    }
+
+    // the last write projected colors and descriptions away
+    locally {
+      val σ = Viewer.read(path)
+      assert(σ.Node.all.forall(_.color == null))
+      assert(σ.Node.all.forall(_.description == null))
+    }
+  }
+
   ignore("create and write nodes with tool 1, append colors and descriptions") {
     val path = tmpFile("nodeExample.create.write.append")
 
