d2e8e4832a2a714ddc8abe4df972e81177a431e1
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-01-14 11:44:14 +0100

    improved node toolchain core test

--- src/test/scala/toolchains/node/CoreTest.scala
+++ src/test/scala/toolchains/node/CoreTest.scala
@@ -27,10 +27,56 @@ class CoreTest extends CommonTest {
     σ.Node(42)
     σ.write(tmpFile("nodeExample.create"))
 
-    σ.Node.all.foreach(println(_))
+    for (n ← σ.Node.all) println(n.prettyString)
   }
 
-  test("create and write nodes with tool 1, append colors and descriptions") {
+  test("create and write nodes with tool 1, append colors and descriptions -- with manual string updates") {
+    val path = tmpFile("nodeExample.with.strings")
+
+    locally {
+      val σ = Creator.create
+      σ.Node(23)
+      σ.Node(42)
+      σ.write(path)
+    }
+
+    locally {
+      import tool2.Node
+
+      val σ = ColorTool.read(path)
+      σ.String.add("red")
+      σ.String.add("black")
+      σ.String.add("grey")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.color = "red"
+        case n @ Node(42, _) ⇒ n.color = "black"
+        case n               ⇒ n.color = "grey"
+      }
+      σ.append
+    }
+
+    locally {
+      import tool3.Node
+
+      val σ = DescriptionTool.read(path)
+      σ.String.add("Some odd number.")
+      σ.String.add("The answer.")
+      σ.String.add("Boring!")
+      σ.Node.all.foreach {
+        case n @ Node(23, _) ⇒ n.description = "Some odd number."
+        case n @ Node(42, _) ⇒ n.description = "The answer."
+        case n               ⇒ n.description = "Boring!"
+      }
+      σ.append
+    }
+
+    locally {
+      val σ = Viewer.read(path)
+      for (n ← σ.Node.all) println(n.prettyString)
+    }
+  }
+
+  ignore("create and write nodes with tool 1, append colors and descriptions") {
     val path = tmpFile("nodeExample.create.write.append")
 
     locally {
