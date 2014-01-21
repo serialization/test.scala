686658cd760ba0a2144f9a01d5d2c7257132bede
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-01-21 13:17:53 +0100

    adapted subtype tests to the newest generator version

---
+++ src/test/scala/subtypes/FullTest.scala
@@ -0,0 +1,32 @@
+package subtypes
+
+import common.CommonTest
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import subtypes.api.SkillState
+
+@RunWith(classOf[JUnitRunner])
+class FullTest extends CommonTest {
+
+  /**
+   * not fully implemented
+   */
+  ignore("delete and write") {
+    val σ = SkillState.read("localBasePoolStartIndex.sf")
+    for (d ← σ.D.all)
+      d.delete
+
+    val path = tmpFile("delete")
+    σ.write(path)
+
+    assert(0 === SkillState.read(path).D.size, "there should be no D")
+  }
+
+  test("delete -- marked") {
+    val σ = SkillState.read("localBasePoolStartIndex.sf")
+    for (d ← σ.D.all)
+      d.delete
+
+    assert(σ.D.all.forall(_.markedForDeletion), "some D is not marked for deletion?!")
+  }
+}
