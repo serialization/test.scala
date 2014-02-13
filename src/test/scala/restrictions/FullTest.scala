81e550d3fdbec463d1f35b13bff2596e3ab3efd9
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-02-13 20:29:56 +0100

    added tests for restrictions

---
+++ src/test/scala/restrictions/FullTest.scala
@@ -0,0 +1,29 @@
+package restrictions
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import common.CommonTest
+
+@RunWith(classOf[JUnitRunner])
+class FullTest extends CommonTest {
+  import restrictions._;
+  import restrictions.range.api.{ SkillState ⇒ Range }
+  import restrictions.singleton.api.{ SkillState ⇒ Singleton }
+
+  test("range") {
+    val p = tmpFile("range")
+    val state = Range.create
+    intercept[range.internal.SkillException] {
+      state.RangeRestricted(0, 0, 0, 0.0f, 0)
+      state.write(p)
+    }
+  }
+
+  test("singleton") {
+    val p = tmpFile("singleton")
+    val state = Singleton.create
+    state.LifeUniverseAndEverything.get.size = Float.NaN
+    state.write(p)
+  }
+}
