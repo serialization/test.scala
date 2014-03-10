b2af437c9fc938e485193cf7ff601b116257b87c
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-03-10 15:35:40 +0100

    fixed paths in data.core test

--- src/test/scala/date/CoreTest.scala
+++ src/test/scala/date/CoreTest.scala
@@ -5,17 +5,19 @@ import junit.framework.Assert
 import date.api.SkillState
 
 class CoreTest extends CommonTest {
+  
+  def read(s:String) = SkillState.read("src/test/resources/"+s)
 
   // read unknown types
-  test("read unknown: nodes") { assert(null != SkillState.read("node.sf")) }
-  test("read unknown: two node blocks") { assert(null != SkillState.read("twoNodeBlocks.sf")) }
-  test("read unknown: colored nodes") { Assert.assertNotNull(SkillState.read("coloredNodes.sf")) }
-  test("read unknown: four colored nodes") { Assert.assertNotNull(SkillState.read("fourColoredNodes.sf")) }
-  test("read unknown: empty blocks") { Assert.assertNotNull(SkillState.read("emptyBlocks.sf")) }
-  test("read unknown: two types") { Assert.assertNotNull(SkillState.read("twoTypes.sf")) }
-  test("read unknown: trivial type definition") { Assert.assertNotNull(SkillState.read("trivialType.sf")) }
-  test("read unknown: nullable restricted null pointer") { SkillState.read("nullableNode.sf").Date.all }
-  test("read unknown: null pointer in an annotation") { SkillState.read("nullAnnotation.sf").Date.all }
+  test("read unknown: nodes") { assert(null != read("node.sf")) }
+  test("read unknown: two node blocks") { assert(null != read("twoNodeBlocks.sf")) }
+  test("read unknown: colored nodes") { Assert.assertNotNull(read("coloredNodes.sf")) }
+  test("read unknown: four colored nodes") { Assert.assertNotNull(read("fourColoredNodes.sf")) }
+  test("read unknown: empty blocks") { Assert.assertNotNull(read("emptyBlocks.sf")) }
+  test("read unknown: two types") { Assert.assertNotNull(read("twoTypes.sf")) }
+  test("read unknown: trivial type definition") { Assert.assertNotNull(read("trivialType.sf")) }
+  test("read unknown: nullable restricted null pointer") { read("nullableNode.sf").Date.all }
+  test("read unknown: null pointer in an annotation") { read("nullAnnotation.sf").Date.all }
 
   // on-demand deserialization
   // TODO
