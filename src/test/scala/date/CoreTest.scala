60923de6ba2a6504b95e8dce90342c434a46518f
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-03-10 17:41:00 +0100

    made date.core tests more useful

--- src/test/scala/date/CoreTest.scala
+++ src/test/scala/date/CoreTest.scala
@@ -7,17 +7,18 @@ import date.api.SkillState
 class CoreTest extends CommonTest {
 
   def read(s: String) = SkillState.read("src/test/resources/"+s)
+  def check(name: String, size: Long) = assert(read(name).Date.size === size)
 
   // read unknown types
-  test("read unknown: nodes") { assert(null != read("node.sf")) }
-  test("read unknown: two node blocks") { assert(null != read("twoNodeBlocks.sf")) }
-  test("read unknown: colored nodes") { Assert.assertNotNull(read("coloredNodes.sf")) }
-  test("read unknown: four colored nodes") { Assert.assertNotNull(read("fourColoredNodes.sf")) }
-  test("read unknown: empty blocks") { Assert.assertNotNull(read("emptyBlocks.sf")) }
-  test("read unknown: two types") { Assert.assertNotNull(read("twoTypes.sf")) }
-  test("read unknown: trivial type definition") { Assert.assertNotNull(read("trivialType.sf")) }
-  test("read unknown: nullable restricted null pointer") { read("nullableNode.sf").Date.all }
-  test("read unknown: null pointer in an annotation") { read("nullAnnotation.sf").Date.all }
+  test("read unknown: nodes")(check("node.sf", 0))
+  test("read unknown: two node blocks")(check("twoNodeBlocks.sf", 0))
+  test("read unknown: colored nodes")(check("coloredNodes.sf", 0))
+  test("read unknown: four colored nodes")(check("fourColoredNodes.sf", 0))
+  test("read unknown: empty blocks")(check("emptyBlocks.sf", 0))
+  test("read unknown: two types")(check("twoTypes.sf", 0))
+  test("read unknown: trivial type definition") { check("trivialType.sf", 0) }
+  test("read unknown: nullable restricted null pointer") { check("nullableNode.sf", 0) }
+  test("read unknown: null pointer in an annotation") { check("nullAnnotation.sf", 2) }
 
   // on-demand deserialization
   // TODO
