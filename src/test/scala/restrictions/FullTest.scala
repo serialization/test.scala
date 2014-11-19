39dc9a1417efaf13079d8a5369257826abf062f3
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2014-11-19 10:20:31 +0100

    fixed a test

--- src/test/scala/restrictions/FullTest.scala
+++ src/test/scala/restrictions/FullTest.scala
@@ -2,28 +2,31 @@ package restrictions
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-
 import common.CommonTest
 
 @RunWith(classOf[JUnitRunner])
 class FullTest extends CommonTest {
   import restrictions._;
-  import restrictions.range.api.{ SkillState ⇒ Range }
-  import restrictions.singleton.api.{ SkillState ⇒ Singleton }
+  import restrictions.range.api.{ SkillFile ⇒ Range }
+  import restrictions.singleton.api.{ SkillFile ⇒ Singleton }
 
-  ignore("range") {
+  test("range") {
+    import restrictions.range.api.Create
+
     val p = tmpFile("range")
-    val state = Range.create
+    val sf = Range.open(p, Create)
     intercept[range.internal.SkillException] {
-      state.RangeRestricted(0.0f, 0, 0, 0, 0)
-      state.write(p)
+      sf.RangeRestricted(0.0f, 0, 0, 0, 0)
+      sf.close
     }
   }
 
   ignore("singleton") {
+    import restrictions.singleton.api.Create
+
     val p = tmpFile("singleton")
-    val state = Singleton.create
-    state.LifeUniverseAndEverything.get.size = Float.NaN
-    state.write(p)
+    val sf = Singleton.open(p, Create)
+    sf.LifeUniverseAndEverything.get.size = Float.NaN
+    sf.close
   }
 }
