e94a3191357ddac10d4d351489e327281cdc3173
Timm Felden <feldentm@informatik.uni-stuttgart.de>
2015-10-23 15:17:41 +0200

    fixed a test and added its degeneration as a regression

--- src/test/scala/date/AppendTest.scala
+++ src/test/scala/date/AppendTest.scala
@@ -170,6 +170,57 @@ class AppendTest extends CommonTest {
         cond &&= (i == d.next.date)
       assert(cond, "match failed")
 
+      state.changePath(writePath)
+      state.close
+    }
+
+    // check append against write
+    locally {
+      val s1 = open(path, Read, ReadOnly)
+      val s2 = open(writePath, Read, ReadOnly)
+
+      val i1 = s1.Date.all
+      val i2 = s2.Date.all
+
+      while (i1.hasNext) {
+        assert(i1.next.date === i2.next.date)
+      }
+
+      assert(i1.hasNext === i2.hasNext, "state1 had less elements!")
+    }
+  }
+
+  test("broken append test -- regression test") {
+    val limit = 1e5.toInt
+    val path = tmpFile("append")
+
+    // write
+    val sf = open(path, Create, Append)
+    for (i ← 0 until limit)
+      sf.Date.make(i)
+
+    sf.flush
+
+    // append
+    for (i ← 1 until 10) {
+      for (v ← i * limit until limit + i * limit)
+        sf.Date.make(v)
+
+      sf.flush
+    }
+
+    // read & check & write
+    val writePath = tmpFile("write")
+    locally {
+      val state = SkillFile.open(path, Read, Write)
+      val d = state.Date.all
+      assert(state.Date.size === 10 * limit, s"we somehow lost ${10 * limit - state.Date.size} dates")
+
+      var cond = true
+      for (i ← 0 until 10 * limit)
+        cond &&= (i == d.next.date)
+      assert(cond, "match failed")
+
       sf.changePath(writePath)
       sf.close
     }
