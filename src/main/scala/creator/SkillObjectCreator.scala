8f57e92ffc18fd13ed449c2afd938a0e869c8b3b
vonzampr <vonzampr@studi.informatik.uni-stuttgart.de>
2017-06-12 16:14:21 +0200

    Add companion Object to SkillObjectCreator

--- src/main/scala/creator/SkillObjectCreator.scala
+++ src/main/scala/creator/SkillObjectCreator.scala
@@ -7,15 +7,17 @@ import de.ust.skill.common.scala.api.SkillObject
 
 class SkillObjectCreator {
 
+}
+
+object SkillObjectCreator {
   def generateSkillFileTypeMappings(sf: SkillFile): Map[String, Access[_]] = {
     val typeMappings: Map[String, Access[_]] = sf.map(t ⇒ t.name -> t).toMap;
     return typeMappings;
   }
 
   def generateSkillFileFieldMappings(sf: SkillFile): Map[String, Map[String, FieldDeclaration[_, _]]] = {
-    val fieldMappings: Map[String, Map[String, FieldDeclaration[_, _]]] = sf.map(t ⇒ (t.name, t.fields.map(f => 
-        (f.name, f.asInstanceOf[FieldDeclaration[_, _ <: SkillObject]])).toMap : Map[String, FieldDeclaration[_, _]] 
-      )).toMap;
+    val fieldMappings: Map[String, Map[String, FieldDeclaration[_, _]]] = sf.map(t ⇒ (t.name, t.fields.map(f =>
+      (f.name, f.asInstanceOf[FieldDeclaration[_, _ <: SkillObject]])).toMap: Map[String, FieldDeclaration[_, _]])).toMap;
     return fieldMappings;
   }
 }
